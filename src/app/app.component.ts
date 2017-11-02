ae46cda3047ae6d5b61c3a223cb60582528a1fa6
merkelAlbert <merkel.albert1997@gmail.com>
2017-11-02 15:56:18 +0300

    добавлен плеер, изменены стили

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import {Component} from '@angular/core';
 
+declare var System: any;
+System.import('./app.component.script.js');
+
+const menuItems = [
+  'Плеер',
+  'Топ песен',
+  'Топ плейлистов',
+  'Новинки'
+
+];
+
 @Component({
   selector: 'app-music',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class MusicAppComponent {
+  menuItems: string[] = menuItems;
 }
